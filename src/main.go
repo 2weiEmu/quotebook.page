0048845d4fae1732f02283e5b6475bca19c74657
2weiEmu <saalbach.robert@outlook.de>
2023-06-24 17:17:34 +0200

    feature: better routing, POST request to add quotes

--- src/main.go
+++ src/main.go
@@ -14,7 +14,7 @@ import (
 
 // Creating the Main Database for Global Access
 // TODO: @robert - this is perhaps not the best way to do this -> but it'll be fine
-var db sql.DB;
+var db *sql.DB;
 var sqlite3Conn sqlite3.SQLiteConn;
 
 
@@ -23,6 +23,10 @@ type QuoteQuery struct {
     Quote string 
     Date string
     Sayer string
+}
+
+type Data struct {
+    Quotes []QuoteQuery
 }
 
 // TODO: @robert one day prepared statements pls
@@ -40,17 +44,6 @@ func getQueryForPage(pageNumber int) string {
 }
 
 func getQuotePageFromDB(pageNumber int) ([]QuoteQuery) {
-
-
-    // connecting to the database here now - not that happy
-    db, connectionError := sql.Open("sqlite3", "file:src/DATABASE?cache=shared")
-
-    if e := db.Ping(); e != nil || connectionError != nil {
-        fmt.Println("Failed to Start the DB")
-    } else {
-        fmt.Println("Connected to SQLite3 Database (DATABASE file)")
-    }
-
     rows, err := db.Query(getQueryForPage(pageNumber))
 
     var returnQuotes []QuoteQuery;
@@ -71,23 +64,47 @@ func getQuotePageFromDB(pageNumber int) ([]QuoteQuery) {
     return returnQuotes
 }
 
-func addQuoteToDb(quote QuoteQuery) {
+func updateHandling(w http.ResponseWriter, req *http.Request) {
 
+
+    //r.FormValue
 }
 
-type Data struct {
-    Quotes []QuoteQuery
-}
+func routeHandler(w http.ResponseWriter, req *http.Request) {
 
-func rootPage(w http.ResponseWriter, req *http.Request) {
-    // This seems a bit... cursed TODO: @robert - fix this some time
-    if match, _ := regexp.MatchString(`/(\?page=[0-9]+)?$`, req.URL.Path); !match {
-        fmt.Println("Handling Path Differently:", req.URL.Path)
+    pathRoute := req.URL.Path 
+
+    if match, _ := regexp.MatchString(`^/(\?page=[0-9]+)?$`, pathRoute); match {
+        indexPage(w, req)
+
+    } else if match, _ := regexp.MatchString(`^/css/`, pathRoute); match {
+        fmt.Println("Serving Static file...")
         fs := http.FileServer(http.Dir("src/static/"))
+        http.StripPrefix("static/", fs)
         fs.ServeHTTP(w, req)
-        return
+
+    } else if match, _ := regexp.MatchString(`^/updates/`, pathRoute); match {
+        updateHandling(w, req)
+
     }
+}
+
+func indexPage(w http.ResponseWriter, req *http.Request) {
     
+    if req.Method == http.MethodPost {
+        fmt.Println("Received update...")
+
+        fmt.Println(req.FormValue("Quote"))
+        quote := req.FormValue("Quote")
+        fmt.Println(req.FormValue("Date"))
+        date := req.FormValue("Date")
+        fmt.Println(req.FormValue("Sayer"))
+        sayer := req.FormValue("Sayer")
+
+        db.Exec("INSERT INTO quotes (quote, date, sayer) VALUES ( ?, ?, ?)", quote, date, sayer)
+
+    }
+
     // Getting the query out of the Request
     queryParams := req.URL.Query()
     pageList := queryParams["page"]
@@ -121,36 +138,17 @@ func rootPage(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 
-
-    /**
-     * DATABASE SETUP
-     */
-
-
-    // !NOTE why the fuck can't I make the database here but have to create the connection also in the rootpage thing? do they have different THREADS?!
-    
-    // file:test.db?cache=shared&mode=memory <- an in-memory database DSN you can use (but not on the Raspi lol)
-    db, connectionError := sql.Open("sqlite3", "file:src/DATABASE?cache=shared")
+    var connectionError error;
+    db, connectionError = sql.Open("sqlite3", "file:src/DATABASE?cache=shared")
 
     if e := db.Ping(); e != nil || connectionError != nil {
         fmt.Println("Failed to Start the DB")
     } else {
         fmt.Println("Connected to SQLite3 Database (DATABASE file)")
     }
-
-    // Close the database once Main exits
-    //defer db.Close()
     
-    /**
-     * HTTP HANDLING
-     */
-    fs := http.FileServer(http.Dir("static/"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-    http.HandleFunc("/", rootPage)
-
-    // TODO: make something that handles the root page, if you go to / and redirects to /quote-list/ or something, and then make the api for the rest
-
+    // HACK: we are actually just doing the jank shit now
+    http.HandleFunc("/", routeHandler)
 
     http.ListenAndServe(":8000", nil)
 }
